20c63ea33ee86f05518e5932cb6b376a9b439f90
Ladislav Smola <lsmola@redhat.com>
2013-11-02 13:49:08 +0100

    Adding code for lesson 1
    Adding Services STI models and it's management

---
+++ app/models/services/advertisement.rb
@@ -0,0 +1,5 @@
+class Advertisement < Service
+  def secret_method
+    "I am super great advertisement"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 LearnRails::Application.routes.draw do
+  resources :services
+  resources :webs
+  resources :advertisements
+
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
